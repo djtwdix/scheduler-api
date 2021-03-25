edd778fb5005b9524280197b7d4756443a44aa8c
djtwdix <djtwd.ix@icloud.com>
2021-03-25 09:03:50 -0300

    Reject unauthorized false

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,8 @@ const pg = require("pg");
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
-  ssl: true
+  ssl: true,
+  rejectUnauthorized: false
 });
 
 client
