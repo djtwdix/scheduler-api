fb3b3773c4fb75fa33f3d7c0c693a257cbf16ba4
djtwdix <djtwd.ix@icloud.com>
2021-03-25 09:05:38 -0300

    add dialect options

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,13 @@ const pg = require("pg");
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
-  ssl: true,
-  rejectUnauthorized: false
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
+
 });
 
 client
